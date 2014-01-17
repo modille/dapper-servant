f0a8b6e01952b02e32d709953893b10f0403fc4b
odillem2 <odillem2@upmc.edu>
2014-01-17 14:25:10 -0500

    Initial commit

---
+++ dapper-servant.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// // @name        Dapper Servant
+// // @namespace   https://github.com/modille
+// // @include     http://*:5050/*/configure
+// // @version     1
+// // @grant       none
+// // ==/UserScript==
+
+
